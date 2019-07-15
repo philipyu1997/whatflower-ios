f8035c712518eb841d5f8bfad23237ced5bc3da0
Philip Yu <pyu3@mail.sfsu.edu>
2019-07-14 18:52:36 -0700

    Set up a UIImagePickerController

--- WhatFlower/Controller/ViewController.swift
+++ WhatFlower/Controller/ViewController.swift
@@ -8,13 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    // Outlets
+    @IBOutlet weak var imageView: UIImageView!
+    
+    // Properties
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+        
     }
 
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
 
 }
 
