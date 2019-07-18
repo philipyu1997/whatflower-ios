6961a8ec8e23e9ba4766843a9b9a2b0775f4fa87
Philip Yu <pyu3@mail.sfsu.edu>
2019-07-17 23:32:22 -0700

    Add Cocoapods and make HTTP GET requests to Wikipedia using Alamofire and REST

--- WhatFlower/Controller/ViewController.swift
+++ WhatFlower/Controller/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 import CoreML
 import Vision
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
@@ -17,6 +19,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     // Properties
     let imagePicker = UIImagePickerController()
+    let wikipediaURL = "https://en.wikipedia.org/w/api.php"
     
     override func viewDidLoad() {
         
@@ -53,9 +56,12 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
         // Handles User request to use in model
         let request = VNCoreMLRequest(model: model) { (request, error) in
-            let classification = request.results?.first as? VNClassificationObservation
+            guard let classification = request.results?.first as? VNClassificationObservation else {
+                fatalError("Could not classify image.")
+            }
             
-            self.navigationItem.title = classification?.identifier.capitalized
+            self.navigationItem.title = classification.identifier.capitalized
+            self.requestInfo(flowerName: classification.identifier)
         }
         
         let handler = VNImageRequestHandler(ciImage: image)
@@ -64,6 +70,32 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             try handler.perform([request])
         } catch {
             print(error)
+        }
+        
+    }
+    
+    func requestInfo(flowerName: String) {
+        
+        let parameters : [String:String] = [
+            "format" : "json",
+            "action" : "query",
+            "prop" : "extracts",
+            "exintro" : "",
+            "explaintext" : "",
+            "titles" : flowerName,
+            "indexpageids" : "",
+            "redirects" : "1",
+        ]
+
+        
+        Alamofire.request(wikipediaURL, method: .get, parameters: parameters).responseJSON { (response) in
+            if response.result.isSuccess {
+                print("Got the wikipedia info.")
+                print(JSON(response.result))
+            } else {
+                print("Failed to get wikipedia info!")
+                print(response)
+            }
         }
         
     }
