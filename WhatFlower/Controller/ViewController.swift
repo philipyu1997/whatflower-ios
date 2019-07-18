13597e55c7b76de90897b66c5ef0e4d90583bb8b
Philip Yu <pyu3@mail.sfsu.edu>
2019-07-17 22:57:06 -0700

    App is able to detect flower type and classify accordingly

--- WhatFlower/Controller/ViewController.swift
+++ WhatFlower/Controller/ViewController.swift
@@ -7,9 +7,11 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-
+    
     // Outlets
     @IBOutlet weak var imageView: UIImageView!
     
@@ -25,12 +27,52 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         imagePicker.allowsEditing = false
         
     }
-
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            guard let ciImage = CIImage(image: userPickedImage) else {
+                fatalError("Cannot convert to CIImage.")
+            }
+            
+            detect(image: ciImage)
+            
+            imageView.image = userPickedImage as? UIImage
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    func detect(image: CIImage) {
+        
+        // Create FlowerClassifier model
+        guard let model =  try? VNCoreMLModel(for: FlowerClassifier().model) else {
+            fatalError("Cannot import model")
+        }
+        
+        // Handles User request to use in model
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            let classification = request.results?.first as? VNClassificationObservation
+            
+            self.navigationItem.title = classification?.identifier.capitalized
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
+        
+    }
+    
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
         
         present(imagePicker, animated: true, completion: nil)
         
     }
-
+    
 }
 
